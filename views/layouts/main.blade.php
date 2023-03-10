a335d06d76b54bdb07bfa7bd69df93a50df34c78
Emeline <emeline.baude3@gmail.com>
2023-03-10 16:08:16 +0100

    test couleur titre bannière

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="{{ get_bloginfo('charset') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="{{ get_stylesheet_directory_uri() }}/assets/css/style.css">
     @head
 </head>
 <body @php(body_class())>
